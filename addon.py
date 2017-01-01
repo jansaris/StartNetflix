8ed6ff1a428dc07c8ee40f58541aafb2f2712e75
Jan Saris <jan.saris@gmail.com>
2017-01-01 14:01:42 +0100

    First version

---
+++ addon.py
@@ -0,0 +1,13 @@
+﻿import subprocess
+import xbmc
+import os
+
+root = os.getcwd().replace(";","")+"\\"
+executable = root+"lib\\NetflixRemoteController.exe"
+
+if xbmc.Player().isPlaying()
+	xbmc.log(msg="Stop current video", level=xbmc.LOGNOTICE)
+	xbmc.Player().stop()
+
+xbmc.log(msg=("Start " + executable), level=xbmc.LOGNOTICE)
+child = subprocess.call([executable, "-k"])
