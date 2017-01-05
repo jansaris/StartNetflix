d49d1293eb96403beb55f61ba8973b8eee6954e0
Jan Saris <jan.saris@gmail.com>
2017-01-05 08:44:04 +0100

    Updated to kodi v17

--- addon.py
+++ addon.py
@@ -1,13 +1,15 @@
 ﻿import subprocess
 import xbmc
-import os
+import xbmcaddon
 
-root = os.getcwd().replace(";","")+"\\"
-executable = root+"lib\\NetflixRemoteController.exe"
+addon = xbmcaddon.Addon(id='script.StartNetflix')
+addondir = xbmc.translatePath(addon.getAddonInfo('path'))
 
-if xbmc.Player().isPlaying()
+executable = addondir + "\\resources\\lib\\NetflixRemoteController.exe"
+
+if xbmc.Player().isPlaying():
 	xbmc.log(msg="Stop current video", level=xbmc.LOGNOTICE)
 	xbmc.Player().stop()
 
 xbmc.log(msg=("Start " + executable), level=xbmc.LOGNOTICE)
-child = subprocess.call([executable, "-k"])
+child = subprocess.call([executable, "smstextentry"])
